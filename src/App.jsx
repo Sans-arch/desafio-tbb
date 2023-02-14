c1914ab0e4a3f32a1c5fab0bb5807308d72a1bc7
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-14 18:37:45 -0300

    Create Global Styles with Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
 function App() {
   return (
-    <Header />
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
 
